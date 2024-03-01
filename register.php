20c76bd4a8ea4a5f6a37975d786bd277b769348a
cima <112912676+Heckseh@users.noreply.github.com>
2024-03-01 09:03:34 +0800

    ukk_narendra
    register

---
+++ register.php
@@ -0,0 +1,84 @@
+<?php
+include "koneksi.php";
+
+if (isset($_POST['login'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+  $namalengkap = $_POST['namalengkap'];
+  $alamat = $_POST['alamat'];
+
+  // echo $username."".$password."".$email."".$namalengkap."".$alamat;
+  $sql=mysqli_query($conn,"insert into user values('','$username','$password','$email','$namalengkap','$alamat')");
+  echo "<script>
+  alert ('register berhasil, silahkan login');
+  location.href='login.php';
+</script> ";
+  
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>REGISTRASI</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="asset/style.css">
+</head>
+
+<body>
+  <?php
+  include "layout/header-login.html";
+  ?>
+
+  <?php
+  include "layout/footer.html";
+  ?>
+
+  <div class="row d-flex justify-content-center  mx-auto" style=" width: 900px; margin: 10px;">
+    <div class=" col-sm-7 p-5 text-white " style="background-color: #005b41;">
+      <h1 class="text-center" style="color:white ;"> REGISTRASI <hr></h1>
+      <form action="register.php" method="POST">
+        <div class="mb-3 mt-3">
+          <label for="username" class="form-label"></label>
+          <input type="text" class="form-control" id="username" placeholder="Enter username" name="username"required>
+        </div>
+        <div class="mb-3 mt-3">
+          <label for="password" class="form-label"></label>
+          <input type="password" class="form-control" id="password" placeholder="Enter password" name="password"required>
+        </div>
+        <div class="mb-3 mt-3">
+          <label for="email" class="form-label"></label>
+          <input type="text" class="form-control" id="email" placeholder="Enter email" name="email"required>
+        </div>
+        <div class="mb-3">
+          <label for="namalengkap" class="form-label"></label>
+          <input type="text" class="form-control" id="namalengkap" placeholder="Enter namalengkap" name="namalengkap"required>
+        </div>
+        <div class="mb-3">
+        <label for="alamat"></label>
+<textarea class="form-control" rows="3" id="comment" placeholder="masukan alamat" name="alamat"required></textarea> 
+        </div>
+        <div class="mb-3">
+          <p>sudah punya akun? <a href="login.php">klik disini</a></p>
+        </div>
+        <div class="col-p-4 text-center">
+          <div class="d-grid">
+        <button type="submit" class="btn btn-dark border" name="login">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+  <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
